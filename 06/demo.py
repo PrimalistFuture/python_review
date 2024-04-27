41a0ecbf20c43102d93f2b85344d8c0ec2a66ec8
Vaughn <vseekamp@gmail.com>
2024-04-27 10:31:09 -0700

    reeborg

---
+++ 06/demo.py
@@ -0,0 +1,9 @@
+# making custom functions
+# def keyword followed by name
+def first_function(some_input):
+    """Triple Quotes for docstrings"""
+    print("Hello")
+    print("Bye")
+
+
+# Reeborg's World
