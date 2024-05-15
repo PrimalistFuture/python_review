ab3e27518f358413d0cab6e1a921a77a187a09f1
Vaughn <vseekamp@gmail.com>
2024-05-15 10:05:38 -0700

    i now have python installed locally...

--- 11-20/14/demo.py
+++ 11-20/14/demo.py
@@ -6,17 +6,20 @@
 # if they get it right, select a new instagramer and compare them
 # be functiony and run the code often
 
+import random
 import game_data
 import art
-import random
+
 
 def get_instagrammer():
     """Returns a random instagrammer from game_data.py"""
     return random.choice(game_data.data)
 
+
 def determine_winner(insta1, insta2, as_string):
     """Takes in two game_data.data dicts, and a T / F.
-    Returns the dict with the larger follower_count, or if T, a string of 'a' or 'b', depending on the follower count."""
+    Returns the dict with the larger follower_count,
+    or if T, a string of 'a' or 'b', depending on the follower count."""
     if insta1["follower_count"] > insta2["follower_count"]:
         if as_string is True:
             return 'a'
@@ -26,9 +29,10 @@ def determine_winner(insta1, insta2, as_string):
             return 'b'
         return insta2
 
+
 def game():
-    print(art.logo)
     """Conducts the game"""
+    print(art.logo)
     print("Welcome to Higher Lower!")
     print("You will see two instragram accounts. Select the account that has more followers.")
     # Initializing variables
@@ -40,7 +44,7 @@ def game():
         # Only gets new instragrammer if one is doesn't already exist
         if a == "":
             a = get_instagrammer()
-        if b == "":    
+        if b == "":
             b = get_instagrammer()
         # checks for duplicates
         while a == b:
@@ -49,27 +53,32 @@ def game():
         # Will evaluate to 'a' or 'b'
         more_followers = determine_winner(a, b, True)
 
-        print(f"Choice a : {a['name']}, a {a['description']} from {a['country']}.")
+        print(f"""Choice a : {a['name']}, a {
+              a['description']} from {a['country']}.""")
         print(art.vs)
-        print(f"Choice b: {b['name']}, a {b['description']} from {b['country']}.")
+        print(f"""Choice b: {b['name']}, a
+              {b['description']} from {b['country']}.""")
 
-        user_choice = input("Who has more instragram followers? Type 'a' or 'b'. ")
+        user_choice = input(
+            "Who has more instragram followers? Type 'a' or 'b'. ")
         # Ends the game on wrong choice
         if user_choice is not more_followers:
-            print(f"Thats incorrect! {a['name']} has {a['follower_count']} followers, and {b['name']} has {b['follower_count']} followers.")
+            print(f"""Thats incorrect! {a['name']} has {a['follower_count']} followers, and {
+                  b['name']} has {b['follower_count']} followers.""")
             game_over = True
             print(f"You have lost with a score of {score}.")
-            return
         else:
-            print(f"Thats correct! {a['name']} has {a['follower_count']} followers, and {b['name']} has {b['follower_count']} followers.")
+            print(f"""Thats correct! {a['name']} has {a['follower_count']} followers, and {
+                  b['name']} has {b['follower_count']} followers.""")
             score += 1
             print(f"You have a score of {score}.")
             # Keeps the winning dict and reset loser variable to fetch a new dict back at the top
             # While we already know who the winner is, they are currently just a string. Probably a sharper way...
-            keep_winner = determine_winner(a,b, False)
+            keep_winner = determine_winner(a, b, False)
             if keep_winner == a:
                 b = ""
             else:
                 a = ""
+
 
 game()
