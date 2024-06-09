51349462f2ecc2fcf38ca42a42107d87686a6e4c
Vaughn <vseekamp@gmail.com>
2024-06-09 09:52:14 -0700

    http requests and headers

---
+++ 31-40/37/demo.py
@@ -0,0 +1,8 @@
+import requests
+
+# POST requests
+# Look the same as get requests but now params are input into json= rather than params=
+
+
+# HEADERS
+# INCLUDE HEADERS AS AN ARG INTO YOUR HTTP REQUESTS
